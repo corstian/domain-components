822d8733003e72891b1cf44e5e1128f54c48b98b
Corstian Boerman <mail@corstianboerman.com>
2022-02-20 11:58:47 +0100

    Organize tests in a way which makes the type matching behaviour explicit.

--- Domain.Example.Tests/TypeMatchingTest.cs
+++ Domain.Example.Tests/TypeMatchingTest.cs
@@ -1,3 +1,4 @@
+using Domain.Components;
 using Domain.Components.Abstractions;
 using Domain.Example.Aggregates.UserAggregate;
 using Domain.Example.Aggregates.UserAggregate.Commands;
@@ -9,16 +10,24 @@ namespace Domain.Example.Tests
     public class TypeMatchingTest
     {
         [Fact]
-        public void Test1()
+        public void MatchingTypes()
         {
-            var command = new ChangeEmail
-            {
-                Email = "john@example.com"
-            };
+            var command = new ChangeEmail();
 
             Assert.True(command is ICommand<User, EmailChanged>);
             Assert.True(command is ICommand<User, IEvent<User>>);
             Assert.True(command is ICommand<User>);
+            Assert.True(command is Command<User, EmailChanged>);
+            
+        }
+
+        [Fact]
+        public void NonMatchingTypes()
+        {
+            var command = new ChangeEmail();
+
+            Assert.False(command is Command<User>);
+            Assert.False(command is Command<User, IEvent<User>>);
         }
     }
 }

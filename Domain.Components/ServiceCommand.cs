fa94fe95a5fe16e0e8fb6ad3a63583026d0bddfa
Corstian Boerman <mail@corstianboerman.com>
2022-03-16 18:12:06 +0100

    Experiment with the construction of an abstract ServiceCommand

---
+++ Domain.Components/ServiceCommand.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Components.Abstractions;
+
+namespace Domain.Components
+{
+    public abstract class ServiceCommand
+    {
+        private CommitPackagesFactory _factory = new();
+
+        public void AddressAggregate<TAggregate>(
+            TAggregate aggregate, 
+            Action<CommitPackageBuilder<TAggregate>> builder)
+            where TAggregate : IAggregate<TAggregate>
+            => _factory.AddCommitPackage(aggregate, builder);
+
+        public void AddressService<TService>(
+            TService service,
+            IServiceCommand<TService> command)
+            where TService : IService<TService>
+            => _factory.AddServiceCommand(service, command);
+
+        public Task<IResult<IEnumerable<ICommitPackage>>> Evaluate() => _factory.Evaluate();
+    }
+}
--- Domain.Example/Services/GroupManagement/RemoveUserFromGroup.cs
+++ Domain.Example/Services/GroupManagement/RemoveUserFromGroup.cs
@@ -1,33 +1,33 @@
 ﻿using Domain.Components;
 using Domain.Components.Abstractions;
 using Domain.Example.Aggregates.GroupAggregate.Commands;
-using Domain.Example.Aggregates.UserAggregate;
 using Domain.Example.Aggregates.UserAggregate.Commands;
 
 namespace Domain.Example.Services.GroupManagement
 {
-    public class RemoveUserFromGroup : IServiceCommand<GroupManagementService>
+    public class RemoveUserFromGroup : ServiceCommand, IServiceCommand<GroupManagementService>
     {
         public Guid GroupId { get; init; }
         public Guid UserId { get; init; }
 
-        async Task<IResult<IEnumerable<ICommitPackage>>> IServiceCommand<GroupManagementService>.Evaluate(GroupManagementService service)
+        Task<IResult<IEnumerable<ICommitPackage>>> IServiceCommand<GroupManagementService>.Evaluate(GroupManagementService service)
         {
             var group = service.GroupRepository.ById(GroupId);
             var user = service.UserRepository.ById(UserId);
 
-            return await new CommitPackagesFactory()
-                .AddCommitPackage(group, groupBuilder => groupBuilder
-                    .IncludeCommand(new RemoveUser
-                    {
-                        UserId = UserId
-                    }))
-                .AddCommitPackage<User>(user, userBuilder => userBuilder
-                    .IncludeCommand(new RemoveGroup
-                    {
-                        GroupId = GroupId
-                    }))
-                .Evaluate();
+            AddressAggregate(group, builder => builder
+                .IncludeCommand(new RemoveUser
+                {
+                    UserId = UserId
+                }));
+
+            AddressAggregate(user, builder => builder
+                .IncludeCommand(new RemoveGroup
+                {
+                    GroupId = GroupId
+                }));
+
+            return Evaluate();
         }
     }
 }
